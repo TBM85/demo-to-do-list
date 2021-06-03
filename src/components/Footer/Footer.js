e1d9854093f400b7feadc4c9b0267f62536a7917
TBM85 <tballesterm@gmail.com>
2021-06-03 12:37:18 -0400

    Include a footer component and apply styles to it

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        © Copyright 2021. <strong>"TBM85"</strong>
+        <br /> All rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
