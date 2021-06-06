31f3bbc5cc9cde544fc3410a949d6cb2adc0b822
TBM85 <tballesterm@gmail.com>
2021-06-06 13:38:17 -0400

    Create a cancel button within the expanded task

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -11,26 +11,42 @@ const TaskItem = (props) => {
   const [isExpand, setIsExpand] = useState(false);
   const expandListHandler = () => {
     setIsExpand(true);
-  }
+  };
 
   const deleteHandler = () => {
     const taskId = props.task.id;
     props.onDeleteTask(taskId);
-  }
+  };
+
+  const cancelHandler = () => {
+    setIsExpand(false);
+  };
 
   return (
-    <li className={`task ${isChecked ? "line-through" : ""}`}>
+    <li className="task">
       <Button
         type="button"
         ariaLabel="Checkmark the task"
         className={`check-btn ${isChecked ? "checked" : ""}`}
         onClick={checkToggleHandler}
       ></Button>
-      <div className="task-item" onClick={expandListHandler}>
-        {props.children}
+      <div className="task-item">
+        <div
+          className={`task-content ${isChecked ? "line-through" : ""}`}
+          onClick={expandListHandler}
+        >
+          {props.children}
+        </div>
         {isExpand && (
           <div className="expand-list">
-            <Button type="button" className="delete-btn" onClick={deleteHandler}/>
+            <Button
+              type="button"
+              className="delete-btn"
+              onClick={deleteHandler}
+            />
+            <Button type="button" onClick={cancelHandler}>
+              Cancel
+            </Button>
           </div>
         )}
       </div>
