59fabee398e73bfe6030458b5695d9eaf5aaee50
TBM85 <tballesterm@gmail.com>
2021-06-08 16:46:17 -0400

    Remove 'hover' over the check btn and icon appear

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -12,10 +12,6 @@ const Btn = styled.button`
   margin: 0.5em 0.25em;
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
     rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
-
-  &:hover {
-    box-shadow: none;
-  }
 `;
 
 const Button = (props) => {
